92f91c42c9f2dab251d4c6a2fee9fd8765ab7a10
philippai2907 <philipp.lehmann@gruppe.ai>
2023-03-27 22:36:09 +0200

    adds methods and function. To Do: ggT

--- src/calculate_on_ECC.py
+++ src/calculate_on_ECC.py
@@ -1,11 +1,13 @@
 # The code executes mathimatical Operations on a vaild Elliptic Curve.
 
 class EllipticCurve:
-    a: int
-    b: int
-    p: int
 
-    def calculatesPointsOnElipticcurve(EC: object) -> list:
+    def __init__(self, a: int, b: int, p:int):
+        self.a: int = a
+        self.b: int = b
+        self.p: int = p
+
+    def calculatesPointsOnElipticcurve(self: object) -> list:
         ListOfPointsOnCurve = []
         for x in range(0, EC.p):
             for y in range(0, EC.p):
@@ -19,11 +21,12 @@ class EllipticCurve:
 
 
 class Point:
-    x: int
-    y: int
+    def __init__(self, x: int, y: int):
+        self.x: int = x
+        self.y: int = y
 
-    def checksIfPointOnCurve(EC: object, P: object) -> bool:
-        if (P.y ^ 2) % EC.p == (P.x ^ 3 + EC.a * P.x + EC.b) % EC.p:
+    def checksIfPointOnCurve(self: object, EC: object) -> bool:
+        if (self.y ^ 2) % EC.p == (self.x ^ 3 + EC.a * self.x + EC.b) % EC.p:
             return True
         else:
             return False
@@ -33,6 +36,35 @@ class Point:
         return None
 
 
+def definesOperationToDo(EC: object, P: object, Q: object) -> bool:
+    if P.x % EC.p == Q.x % EC.p and P.y % EC.p == Q.y % EC.p:
+        return True
+    else:
+        return False
+    
+
+def doubleThePoint(EC, P):
+    firsthalf = (3 * P.x ^ 2 + EC.a) % EC.p
+    seconedhalf = (2 * P.y * 1) % EC.p
+    s = firsthalf * advencedEuclidAlgorithm(EC.p, seconedhalf) % EC.p
+
+
+def advencedEuclidAlgorithm(x: int, y: int) -> int:
+    ggTSteps = ggT(x, y)
+
+
+def ggT(x: int, y: int) -> list:
+    ggTSteps = [x, y]
+    if x >= y and y != 0:
+        z = x % y
+        z.append(z)
+        ggT(y, z)
+    return ggTSteps
+
+
+
 EC = EllipticCurve(3, 6, 17)
 P = Point(1, 17)
-print(EC.a)
+Q = Point(3, 9)
+print(P.checksIfPointOnCurve(EC))
+print(definesOperationToDo(EC, P, Q))
