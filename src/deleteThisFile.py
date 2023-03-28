62751b30c5ff60fa1e179275a8f3b7a621b93a5f
philippai2907 <philipp.lehmann@gruppe.ai>
2023-03-28 22:39:27 +0200

    code works right. User interaction is missing

--- src/deleteThisFile.py
+++ src/deleteThisFile.py
@@ -1,15 +1,44 @@
-def advencedEuclidAlgorithm(x: int, y: int) -> int:
-    ggTSteps = ggT(x, y)
-    print(ggTSteps)
+#
+
+import math
+
+
+def advencedEuclidAlgorithm(EC: object, x: int, y: int) -> int:
+    qi = ggT(x, y)
+    print("This is ggT: " + str(qi))
+    ri = []
+    ti = [0, 1]
+    for i in range(len(qi) - 2):
+        ri.append(math.floor(qi[i] / qi[i+1]))
+    print("This is ri:  " + str(ri))
+    for i in range(len(ri)):
+        ti.append(ti[i] - ti[i+1] * ri[i])
+    print("This is ti:  " + str(ti))
+    while ti[len(ti)-1] <= 0:
+        ti[len(ti)-1] + EC.p
+    print(ti[len(ti)-1])
+    return(ti[len(ti)-1])
+    
+
+
 
 def ggT(x, y) -> list:
-    ggTSteps = [x, y]
-    if x >= y and y != 0:
+    ggTSteps = []
+    ggTSteps.append(x)
+    ggTSteps.append(y)
+    while x >= y and y != 0:
         z = x % y
         ggTSteps.append(z)
-        ggT(y, z)
-    print(ggTSteps)
+        x = y
+        y = z
     return ggTSteps
+
+#    if x >= y and y != 0:
+#        z = x % y
+#        ggTSteps.append(z)
+#        ggT(y, z)
+#    print(ggTSteps)
+#    return ggTSteps
 
 
 x = 19
