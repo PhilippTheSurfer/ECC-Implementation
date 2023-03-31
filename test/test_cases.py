1db1410acf9ea3df4c8eeeaa3bbbdea1eec4c5f2
philippai2907 <philipp.lehmann@gruppe.ai>
2023-03-31 10:39:19 +0200

    kein ernsthafter pull request

--- test/test_cases.py
+++ test/test_cases.py
@@ -1,12 +1,34 @@
+import sys
 import os
-print(os.path)
-# import .src.calculate_on_ECC as ECC
-#from src import calculate_on_ECC as ECC
+
+# Add the parent folder to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src import calculate_on_ECC as ECC
+
 
 def test():
-    EC = ECC.EllipticCurve(12, 4, 23)
+    ec = ECC.EllipticCurve(12, 4, 23)
     P = ECC.Point(0, 2)
-    if P.checksIfPointOnCurve(EC=EC):
+    if P.checksIfPointOnCurve(ec=ec):
         print("valid")
     else:
         print("false")
+
+
+test()
+
+def test_2():
+    ec = ECC.EllipticCurve(12, 4, 22)
+    P = ECC.Point(0, 5)
+    if P.checksIfPointOnCurve(ec=ec):
+        print("valid")
+    else:
+        print("false")
+
+
+test_2()
+    #
+    #pytest: param
+    #    viele Punkte die druchgetestet werden.
+    #
